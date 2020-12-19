1a399fe0215c2ad7d9d529449eb717ce7453bde9
kolbymckeown <mckeownkolby@gmail.com>
2020-12-19 18:57:14 -0500

    Basic Styling + Logo

---
+++ coding-the-dark-arts/components/ui/footer/index.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+	return (
+	  <footer className="footer">
+		<a
+		  href="https://github.com/andrewbaldwin44/Coding_The_Dark_Arts"
+		  target="_blank"
+		  rel="noopener noreferrer"
+		>
+		  Powered by CDA
+		</a>
+	  </footer>
+	);
+  };
+  
+  export default Footer;
+  
--- coding-the-dark-arts/pages/_app.js
+++ coding-the-dark-arts/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import PropTypes from "prop-types";
+import GlobalStyles from "../styles/GlobalStyles";
+import Layout from "../components/layout";
+// import { gql } from "@apollo/client";
+
+MyApp.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object,
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
