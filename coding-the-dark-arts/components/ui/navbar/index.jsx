2a20f6e1f655a25e6e423758ac66a85d34c5477d
kolbymckeown <mckeownkolby@gmail.com>
2020-12-23 01:39:21 -0500

    Login Page Basic Styling

--- coding-the-dark-arts/components/ui/navbar/index.jsx
+++ coding-the-dark-arts/components/ui/navbar/index.jsx
@@ -1,61 +1,68 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import { connect } from 'react-redux'
-import { initiateLogout } from '../../../auth/firebase-actions'
+import { connect } from "react-redux";
+import { initiateLogout } from "../../../auth/firebase-actions";
 
 const Navbar = ({ initiateLogout, userData }) => {
-  React.useEffect(() => {
-    console.log(userData)
-  }, [userData])
-  
-  const [menu, setMenu] = useState(false);
-  const toggleMenu = () => {
-    setMenu(!menu);
-  };
+	React.useEffect(() => {
+		console.log(userData);
+	}, [userData]);
 
-  return (
-    <>
-      <nav className="c-navbar">
-        <p style={{ display: !menu ? "flex" : "none" }}>
-          Welcome to the Dark Arts
-        </p>
-        <div style={{ display: menu ? "flex" : "none" }}>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          {/* Next.js uses Link to wrap an <a> tag which allows for users to navigate between pages - The code above will return the user to the home page located at localhost:3000/ */}
-          <Link href="/">
-            <a>Posts</a>
-          </Link>
-          {/* Temp href for the time being until pages are set up */}
-          <Link href="/">
-            <a>About</a>
-          </Link>
-          <button onClick={initiateLogout}>Log Out</button>
-        </div>
-        <button
-          className="c-button"
-          style={{
-            position: "absolute",
-            backgroundColor: "transparent",
-            border: "none",
-          }}
-          onClick={toggleMenu}
-        >
-          {!menu && <span className="c-button-icon">&#8594;</span>}
-          {/* <MenuIcon /> */}
-        </button>
-      </nav>
-    </>
-  );
+	const handleLoginRedirect = () => {
+		console.log("hello");
+	};
+
+	const [menu, setMenu] = useState(false);
+	const toggleMenu = () => {
+		setMenu(!menu);
+	};
+
+	return (
+		<>
+			<nav className="c-navbar">
+				<p style={{ display: !menu ? "flex" : "none" }}>
+					Welcome to the Dark Arts
+				</p>
+				<div style={{ display: menu ? "flex" : "none" }}>
+					<Link href="/">
+						<a>Home</a>
+					</Link>
+					{/* Next.js uses Link to wrap an <a> tag which allows for users to navigate between pages - The code above will return the user to the home page located at localhost:3000/ */}
+					<Link href="/">
+						<a>Posts</a>
+					</Link>
+					{/* Temp href for the time being until pages are set up */}
+					<Link href="/">
+						<a>About</a>
+					</Link>
+					{userData ? (
+						<a onClick={initiateLogout}>Logout</a>
+					) : (
+						<Link href="/login">Login</Link>
+					)}
+				</div>
+				<button
+					className="c-button"
+					style={{
+						position: "absolute",
+						backgroundColor: "transparent",
+						border: "none",
+					}}
+					onClick={toggleMenu}
+				>
+					{!menu && <span className="c-button-icon">&#8594;</span>}
+					{/* <MenuIcon /> */}
+				</button>
+			</nav>
+		</>
+	);
 };
 
 export default connect(
-  state => ({
-    userData: state.firebase.userData,
-  }),
-  {
-    initiateLogout
-  }
+	(state) => ({
+		userData: state.firebase.userData,
+	}),
+	{
+		initiateLogout,
+	}
 )(Navbar);
-
--- coding-the-dark-arts/pages/login/index.js
+++ coding-the-dark-arts/pages/login/index.js
@@ -5,63 +5,70 @@ import { connect } from "react-redux";
 import { initiateLogin, initiateRegister } from "../../auth/firebase-actions";
 
 function Login({ initiateLogin, userData, initiateRegister }) {
-  const emailField = createRef(null);
-  const passwordField = createRef(null);
-  const createEmailField = createRef(null);
-  const createPasswordField = createRef(null);
+	const emailField = createRef(null);
+	const passwordField = createRef(null);
+	const createEmailField = createRef(null);
+	const createPasswordField = createRef(null);
 
-  React.useEffect(() => {
-    console.log(userData);
-  }, [userData]);
-  const handleSubmit = (event) => {
-    event.preventDefault();
+	React.useEffect(() => {
+		console.log(userData);
+	}, [userData]);
+	const handleSubmit = (event) => {
+		event.preventDefault();
 
-    const email = emailField.current.value;
-    const password = passwordField.current.value;
+		const email = emailField.current.value;
+		const password = passwordField.current.value;
 
-    initiateLogin({ email, password });
-  };
+		initiateLogin({ email, password });
+	};
 
-  const handleCreateAccountSubmit = (event) => {
-    event.preventDefault();
-    // TODO: Handle creating account
-    const createEmail = createEmailField.current.value;
-    const createPassword = createPasswordField.current.value;
-    // console.log(createEmail, createPassword)
-    initiateRegister({ email: createEmail, password: createPassword });
-  };
+	const handleCreateAccountSubmit = (event) => {
+		event.preventDefault();
+		// TODO: Handle creating account
+		const createEmail = createEmailField.current.value;
+		const createPassword = createPasswordField.current.value;
+		// console.log(createEmail, createPassword)
+		initiateRegister({ email: createEmail, password: createPassword });
+	};
 
-  if (userData) {
-    Router.push("/");
-    return null;
-  }
+	if (userData) {
+		Router.push("/");
+		return null;
+	}
 
-  return (
-    <>
-      <fieldset>
-        <form onSubmit={handleSubmit}>
-          <input type="text" ref={emailField} />
-          <input type="password" ref={passwordField} />
-          <button type="submit">Login</button>
-        </form>
-      </fieldset>
-      <fieldset>
-        <form onSubmit={handleCreateAccountSubmit}>
-          <input type="text" ref={createEmailField} />
-          <input type="password" ref={createPasswordField} />
-          <button type="submit">Create Account</button>
-        </form>
-      </fieldset>
-    </>
-  );
+	return (
+		<div className="login-container">
+			<div className="login-wrapper">
+        <h1>Welcome Back!</h1>
+				<fieldset>
+					<form className="form" onSubmit={handleSubmit}>
+						<input type="text" placeholder="Email" ref={emailField} />
+						<input type="password" placeholder="Password" ref={passwordField} />
+						<button type="submit">Login</button>
+					</form>
+				</fieldset>
+			</div>
+			<div className="create-account-wrapper">
+      <h1>First Time Here?</h1>
+      <h2>Sign on up!</h2>
+				<fieldset>
+					<form className="form" onSubmit={handleCreateAccountSubmit}>
+						<input type="text" placeholder="Email" ref={createEmailField} />
+						<input type="password" placeholder="Password" ref={createPasswordField} />
+						<button type="submit">Create Account</button>
+					</form>
+				</fieldset>
+			</div>
+		</div>
+	);
 }
 
 export default connect(
-  (state) => ({
-    userData: state.firebase.userData,
-  }),
-  {
-    initiateLogin,
-    initiateRegister,
-  }
+	(state) => ({
+		userData: state.firebase.userData,
+	}),
+	{
+		initiateLogin,
+		initiateRegister,
+	}
 )(Login);
