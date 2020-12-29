520bcf410b3b1254d161fb7439829a9d876638bd
Kolby McKeown <62035359+kolbymckeown@users.noreply.github.com>
2020-12-29 01:46:30 -0500

    Dynamic Routing (#10)
    * Dynamic Routes + Basic Styling

--- coding-the-dark-arts/pages/posts/[slug].js
+++ coding-the-dark-arts/pages/posts/[slug].js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import React from "react";
 import gql from "graphql-tag";
 import ApolloClient from "apollo-boost";
 
@@ -7,19 +8,58 @@ const client = new ApolloClient({
 });
 
 export default function individualPost() {
-	// const [data, setData] = React.useState(null);
+	const [data, setData] = React.useState(null);
+	// TODO: get BlogPost id based off slug from url params
+	React.useEffect(() => {
+		client
+			.query({
+				query: gql`
+					query BlogPost {
+						BlogPost(id: "f13ce4b7-ae28-4406-a955-bb75efa4aaad") {
+							postTitle
+							postDescription
+							postContent
+							image {
+								asset {
+									url
+								}
+							}
+						}
+					}
+				`,
+			})
+			.then((info) => setData(info.data.BlogPost))
+			.catch((error) => console.error(error));
+	}, []);
 
-	// React.useEffect(() => {
-	// query individual post data based on slug
-	// postTitle, postImage, postConent
-	// client
-	// 	.query({
-	// 		query: gql`
+	if (!data) {
+		return (
+			<>
+				<Head>
+					<title>Loading...</title>
+				</Head>
+				<div>Loading...</div>;
+			</>
+		);
+	}
 
-	// 		`,
-	// 	})
-	// .then((info) => setData())
-	// .catch((error) => console.error(error));
-	// }, []);
-	return <div>Individual Post</div>;
+	return (
+		<>
+			<Head>
+				<title>{data.postTitle}</title>
+			</Head>
+			<div className="single-post-wrapper">
+				<div>
+					<h1>{data.postTitle}</h1>
+					<h2>{data.postDescription}</h2>
+				</div>
+				<div className="single-post-img">
+					<img src={data.image.asset.url} />
+				</div>
+				<div>
+					<p>{data.postContent}</p>
+				</div>
+			</div>
+		</>
+	);
 }
