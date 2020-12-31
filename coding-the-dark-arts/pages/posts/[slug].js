a55f8ce777740d56d3aae36ce92507a600aad91b
kolbymckeown <mckeownkolby@gmail.com>
2020-12-31 03:03:16 -0500

    Individual Blog Post Page Rendering

--- coding-the-dark-arts/pages/posts/[slug].js
+++ coding-the-dark-arts/pages/posts/[slug].js
@@ -2,35 +2,51 @@ import Head from "next/head";
 import React from "react";
 import gql from "graphql-tag";
 import ApolloClient from "apollo-boost";
+import { useRouter } from "next/router";
 
 const client = new ApolloClient({
 	uri: `${process.env.NEXT_PUBLIC_URI}`,
 });
 
 export default function individualPost() {
+	// This test function is literally here only for the useEffect callback, it does nothing else
+	
+	const test = () => {
+		console.log('hello')
+	}
 	const [data, setData] = React.useState(null);
-	// TODO: get BlogPost id based off slug from url params
+	
+	const { query } = useRouter();
+	// since slug is initially undefined for half a second, we cannot query based of it
+	// but since the useEffect rerenders based off when the "test" function is defined
+	// then the slug is available and we can render the data based on it
+	const slug = query.slug;
+	console.log(slug);
+	const postQuery = gql`
+		query BlogPost($slug: String!) {
+			allBlogPost(where: { slug: { current: { eq: $slug } } }) {
+				postTitle
+				postContent
+				postDescription
+				image {
+					asset {
+						url
+					}
+				}
+			}
+		}
+	`;
 	React.useEffect(() => {
 		client
 			.query({
-				query: gql`
-					query BlogPost {
-						BlogPost(id: "f13ce4b7-ae28-4406-a955-bb75efa4aaad") {
-							postTitle
-							postDescription
-							postContent
-							image {
-								asset {
-									url
-								}
-							}
-						}
-					}
-				`,
+				query: postQuery,
+				variables: {
+					slug: slug,
+				},
 			})
-			.then((info) => setData(info.data.BlogPost))
+			.then((info) => setData(info.data.allBlogPost[0]))
 			.catch((error) => console.error(error));
-	}, []);
+	}, [test]);
 
 	if (!data) {
 		return (
