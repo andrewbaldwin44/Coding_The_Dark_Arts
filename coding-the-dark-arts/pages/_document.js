448876d451a740cb13ee7d3f5b332134f0d607df
Andrew Baldwin <andrew.baldwin44@gmail.com>
2021-01-02 23:33:29 -0500

    Initial styling of home page

--- coding-the-dark-arts/pages/_document.js
+++ coding-the-dark-arts/pages/_document.js
@@ -10,15 +10,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;1,400;1,700&display=swap'
-            rel='stylesheet'
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Redressed&display=swap'
-            rel='stylesheet'
-          />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&family=Redressed&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
--- coding-the-dark-arts/pages/index.js
+++ coding-the-dark-arts/pages/index.js
@@ -1,21 +1,47 @@
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import React from 'react';
 
 export default function Home() {
+  const icons = [
+    { src: '/javascript.svg', alt: 'JavaScript' },
+    { src: '/react.svg', alt: 'React.js' },
+    { src: '/redux.svg', alt: 'Redux.js' },
+    { src: '/next-dot-js.svg', alt: 'Next.js' },
+    { src: '/sass.svg', alt: 'Sass' },
+  ];
+
   return (
     <>
       <Head>
         <title>Coding The Dark Arts</title>
       </Head>
-      <div className='c-container'>
+      <div className='c-home__heading-container'>
         <h1>Coding The Dark Arts</h1>
-        <h2>With the level 14 JS Wizards</h2>
-        <ul>
-          <li>Andrew</li>
-          <li>Kolby</li>
-        </ul>
-        <Image height='175' width='175' src='/wizard.png' />
+        <h2>With Andrew and Kolby</h2>
+        <h3 className='c-home__blog-redirect'>
+          Go to the Blog
+          <Image src='/arrow.svg' alt='Arrow Icon' height='30px' width='30px' />
+        </h3>
+      </div>
+      <div className='c-home__icon-container'>
+        {icons.map(({ src, alt }, index) => {
+
+
+          return (
+            <div key={`home-icon-${index}`} className='c-home__icon-square'>
+              <div>
+                <Image
+                  className='c-home__icon'
+                  src={src}
+                  alt={alt}
+                  height='80px'
+                  width='80px'
+                />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
