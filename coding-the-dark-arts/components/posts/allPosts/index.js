e8e8e878d93d16ab3d80a694ad91c2a7ca70d383
kolbymckeown <mckeownkolby@gmail.com>
2021-01-04 22:51:56 -0500

    refactored post pages

---
+++ coding-the-dark-arts/components/posts/allPosts/index.js
@@ -0,0 +1,76 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import gql from 'graphql-tag';
+import ApolloClient from 'apollo-boost';
+import React from 'react';
+import Wrapper from '../../postsStyle/Wrapper';
+import SearchWidget from '../../searchWidget';
+
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_URI}`,
+});
+
+export default function AllPosts() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query allBlogPost {
+            allBlogPost {
+              postTitle
+              postContent
+              postDescription
+              image {
+                asset {
+                  url
+                }
+              }
+              slug {
+                current
+              }
+            }
+          }
+        `,
+      })
+      .then(info => setData(info.data.allBlogPost))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>All Posts</title>
+      </Head>
+
+      {console.log(process.env.NEXT_PUBLIC_URI)}
+      <div className='all-post-wrapper'>
+        <h1>All Posts</h1>
+        <div className='search-component'>
+          <SearchWidget />
+        </div>
+        <div className='all-posts-cont'>
+          {data &&
+            data.map(dataPiece => {
+              return (
+                // redirects based on which post is clicked
+                <div className='c-post__wrapper'>
+                  <Link href={`/posts/${dataPiece.slug.current}`}>
+                    <a>
+                      <Wrapper key={`post-title-${dataPiece.postTitle}`}>
+                        <h1>{dataPiece.postTitle}</h1>
+                        <h2>{dataPiece.postDescription}</h2>
+                        <img className='dataPiece-sml' src={dataPiece.image.asset.url} />
+                      </Wrapper>
+                    </a>
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ coding-the-dark-arts/components/posts/individualPost/index.js
@@ -0,0 +1,81 @@
+import Head from 'next/head';
+import React from 'react';
+import gql from 'graphql-tag';
+import ApolloClient from 'apollo-boost';
+import { useRouter } from 'next/router';
+import Markdown from '../../markdown';
+
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_URI}`,
+});
+
+export default function individualPost() {
+  // This test function is literally here only for the useEffect callback, it does nothing else
+
+  const mounted = () => {
+    return;
+  };
+
+  const [data, setData] = React.useState(null);
+
+  const { query } = useRouter();
+  // since slug is initially undefined for half a second, we cannot query based of it
+  // but since the useEffect rerenders based off when the "test" function is defined
+  // then the slug is available and we can render the data based on it
+  const slug = query.slug;
+  console.log(slug);
+  const postQuery = gql`
+    query BlogPost($slug: String) {
+      allBlogPost(where: { slug: { current: { eq: $slug } } }) {
+        postTitle
+        postContent
+        postDescription
+        image {
+          asset {
+            url
+          }
+        }
+      }
+    }
+  `;
+  React.useEffect(() => {
+    client
+      .query({
+        query: postQuery,
+        variables: {
+          slug: slug,
+        },
+      })
+      .then(info => setData(info.data.allBlogPost[0]))
+      .catch(error => console.error(error));
+  }, [mounted]);
+
+  if (!data) {
+    return (
+      <>
+        <Head>
+          <title>Loading...</title>
+        </Head>
+        <div>Loading...</div>;
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{data.postTitle}</title>
+      </Head>
+      <div className='c-single-post__wrapper'>
+        <div>
+          <h1>{data.postTitle}</h1>
+          <h2>{data.postDescription}</h2>
+        </div>
+        <div className='single-post-img'>
+          <img src={data.image.asset.url} />
+        </div>
+        <Markdown children={data.postContent} />
+      </div>
+    </>
+  );
+}
